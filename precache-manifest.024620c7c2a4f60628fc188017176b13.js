3c5ef55a9ff8b6d4822c87a27d5eb5010cec8613
Richard Smith <rsmith37@ggc.edu>
2019-09-08 19:11:18 -0400

    Updates

---
+++ precache-manifest.024620c7c2a4f60628fc188017176b13.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "279206e3afc9d76879a1568af60e9e3e",
+    "url": "/contactmanager/index.html"
+  },
+  {
+    "revision": "eddcd362fdf31a385881",
+    "url": "/contactmanager/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "609522bc918973daa23d",
+    "url": "/contactmanager/static/css/main.36497136.chunk.css"
+  },
+  {
+    "revision": "eddcd362fdf31a385881",
+    "url": "/contactmanager/static/js/2.4d06e3e6.chunk.js"
+  },
+  {
+    "revision": "609522bc918973daa23d",
+    "url": "/contactmanager/static/js/main.891ec2cc.chunk.js"
+  },
+  {
+    "revision": "440bac45752b04fd4578",
+    "url": "/contactmanager/static/js/runtime~main.43891527.js"
+  }
+]);
